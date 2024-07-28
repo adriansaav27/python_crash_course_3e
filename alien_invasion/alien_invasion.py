00304ba6f199e162d9a917efc713dc53713d0157
adriansaav27 <adriansaav27@gmail.com>
2024-07-28 20:15:06 +0200

    Alien Invasion

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -12,9 +12,10 @@ class AlienInvasion:
         pygame.init()
         self.clock = pygame.time.Clock()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height)
-        )
+
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
 
         pygame.display.set_caption("Alien Invasion")
 
@@ -27,7 +28,8 @@ class AlienInvasion:
     def run_game(self):
         while True:
             self._check_events()
-            self._update_scren()
+            self.ship.update()
+            self._update_screen()
             self.clock.tick(60)
 
     # Mira los eventos del teclado y ratón.
@@ -35,9 +37,29 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    # Responde al pulsar las teclas.
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+
+    # Responde al dejar de pulsar las teclas.
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+        elif event.key == pygame.K_q:
+            sys.exit()
 
     # Vuelva a dibujar la pantalla durante cada paso por el bucle.
-    def _update_scren(self):
+    def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
 
--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -7,6 +7,7 @@ class Ship:
     # Inicializa la nave y establece su posición inicial.
     def __init__(self, ai_game):
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Carga la imagen de la nave.
@@ -16,6 +17,26 @@ class Ship:
         # Comienza cada nueva nave en la parte inferior central de la pantalla.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Guarda el float de la posición exacta de la nave.
+        self.x = float(self.rect.x)
+
+        # Bandera de movimiento, comienza con el barco que no está en movimiento.
+        self.moving_right = False
+        self.moving_left = False
+
     # Pinta la nave en la posición inicial.
     def blitme(self):
         self.screen.blit(self.image, self.rect)
+
+    # Actualiza la posición del barco según la bandera de movimiento.
+    def update(self):
+        # Actualiza el valor ´x´ de la nave.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.x += 1
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        # Actualiza el rect, desde el valor de la x.
+        self.rect.x = self.x
